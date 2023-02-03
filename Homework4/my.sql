867b27c6699b003d36555b967eefe3395f3a9b04
Roman100500 <chizhkinra001@mail.ru>
2023-02-03 14:58:29 +0300

    Homework4 complete

---
+++ Homework4/my.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS  information(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  address TEXT NOT NULL
+);
+
+INSERT INTO information (name, age, address)
+VALUES ('Petya', 25, 'Moscow, Russia');
+INSERT INTO information (name, age, address)
+VALUES ('Roman', 22, 'Volgograd, Russia');
+INSERT INTO information (name, age, address)
+VALUES ('Denis', 27, 'Dubai, UAE');
+INSERT INTO information (name, age, address)
+VALUES ('Victoria', 30, 'Moscow, Russia');
+INSERT INTO information (name, age, address)
+VALUES ('Jonh', 37, 'Cordoba, Argentina');
+INSERT INTO information (name, age, address)
+VALUES ('Vasya', 28, 'Sochi, Russia');
+INSERT INTO information (name, age, address)
+VALUES ('Alex', 25, 'Moscow, Russia');
+INSERT INTO information (name, age, address)
+VALUES ('Tanya', 17, 'Moscow, Russia');
+
+SELECT name FROM information
+WHERE address LIKE '%Moscow%' AND age >= 18 AND age < 30
+
+-- Делал в http://sqlfiddle.com (MySQL5.6)
